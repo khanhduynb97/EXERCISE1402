574a14e789bb597d7161f9958ff3e92498889348
duynkth1807054@fpt.edu.vn <duynkth1807054@fpt.edu.vn>
2023-02-15 17:40:45 +0700

    init commit 3

--- app/src/main/java/com/example/exercise1402/MainActivity.java
+++ app/src/main/java/com/example/exercise1402/MainActivity.java
@@ -3,13 +3,19 @@ package com.example.exercise1402;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView tvDesc;
+    Button btnPlay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_movie);
+
+        tvDesc = findViewById(R.id.tvDesc);
+        btnPlay = findViewById(R.id.btnPlay);
     }
 }
