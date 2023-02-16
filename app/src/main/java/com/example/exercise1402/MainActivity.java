0c701782f8a0c57c6fc641b40a2017903f62a263
duynkth1807054@fpt.edu.vn <duynkth1807054@fpt.edu.vn>
2023-02-16 09:39:30 +0700

    init commit done

--- app/src/main/java/com/example/exercise1402/MainActivity.java
+++ app/src/main/java/com/example/exercise1402/MainActivity.java
@@ -7,15 +7,11 @@ import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    TextView tvDesc;
-    Button btnPlay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie);
 
-        tvDesc = findViewById(R.id.tvDesc);
-        btnPlay = findViewById(R.id.btnPlay);
     }
 }
